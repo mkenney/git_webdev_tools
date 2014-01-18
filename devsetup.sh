013d7a9ae7b734b5b4473204593fceda449a92b2
Michael Kenney <michael.kenney@returnpath.net>
2014-01-18 01:49:36 -0700

    Readme, gerrit git hook

--- devsetup.sh
+++ devsetup.sh
@@ -13,8 +13,8 @@ script_path=$(cd `dirname "$0"` && pwd -P)
 script_name=$(basename ${0})
 
 # List required sub-modules
-SUBMODULE_NAME[1]="cert_build_web"
-SUBMODULE_PATH[1]="build-tools"
+SUBMODULE_URL[1]="https://github.com/mkenney/git_webdev_tools.git"
+SUBMODULE_PATH[1]="git-webdev-tools"
 
 # whereami
 CURRENT_MODULE=`git remote -v | tail -1 | cut -d' ' -f 1 | rev | cut -d'/' -f 1 | rev`
@@ -40,22 +40,19 @@ if [[ "${1}" != "updated" ]]; then
 	echo -e " Adding missing submodules... \c"
 	# remove existing modules from the list
 	while read module; do
-		for (( i=1; i<=${#SUBMODULE_NAME[@]}; i++ )); do
-			if [[ "${SUBMODULE_NAME[$i]}" == "${module}" ]]; then
-				SUBMODULE_NAME[$i]=""
+		for (( i=1; i<=${#SUBMODULE_URL[@]}; i++ )); do
+			if [[ "${SUBMODULE_URL[$i]}" == "${module}" ]]; then
+				SUBMODULE_URL[$i]=""
 				SUBMODULE_PATH[$i]=""
 			fi
 		done
 	done <<< "$(git submodule --quiet foreach "git remote -v | tail -1 | cut -d' ' -f 1 | cut -d':' -f 2 | rev | sed 's/^\/*//g' | cut -d'/' -f 1 | rev")"
 	# add all remaining submodules
-	for (( i=1; i<=${#SUBMODULE_NAME[@]}; i++ )); do
-		if [[ "${SUBMODULE_NAME[$i]}" != "" ]] && [[ "${SUBMODULE_NAME[$i]}" != "${CURRENT_MODULE}" ]]; then
+	for (( i=1; i<=${#SUBMODULE_URL[@]}; i++ )); do
+		if [[ "${SUBMODULE_URL[$i]}" != "" ]] && [[ "${SUBMODULE_URL[$i]}" != "${CURRENT_MODULE}" ]]; then
 			echo
-			echo -e "	Adding ${SUBMODULE_NAME[$i]} --> ${SUBMODULE_PATH[$i]}... \c"
-			command git submodule add ssh://gerrit/${SUBMODULE_NAME[$i]} ${SUBMODULE_PATH[$i]}
-			echo Done.
-			echo -e "		Adding commit-msg hook for Gerrit Commit-Ids... \c"
-			scp -p -P 29418 gerrit:hooks/commit-msg ${SUBMODULE_PATH[$i]}/.git/hooks/
+			echo -e "	Adding ${SUBMODULE_URL[$i]} --> ${SUBMODULE_PATH[$i]}... \c"
+			command git submodule add ${SUBMODULE_URL[$i]} ${SUBMODULE_PATH[$i]}
 			echo Done.
 			echo "	-------------------------------------------------------"
 		fi
@@ -64,13 +61,13 @@ if [[ "${1}" != "updated" ]]; then
 	echo " -------------------------------------------------------"
 
 
-	if [ -f "${script_path}/build-tools/devsetup.sh" ]; then
+	if [ -f "${script_path}/git-webdev-tools/devsetup.sh" ]; then
 		echo " *** Updating THIS script (${script_path}/${script_name}) from master... ***"
 		echo " -------------------------------------------------------"
-		cd ${script_path}/build-tools
+		cd ${script_path}/git-webdev-tools
 		git checkout master && git pull
 		cd ${script_path}
-		command cp --remove-destination build-tools/devsetup.sh ${script_name}
+		command cp --remove-destination git-webdev-tools/devsetup.sh ${script_name}
 		echo Done.
 		echo " -------------------------------------------------------"
 		echo " Restarting..."
@@ -87,20 +84,26 @@ fi
 #	checkout modules in devsetup.conf and run all devsetup.sh scripts
 
 echo -e " Setting up pre-commit hook... \c"
-cd ${script_path} && command ln -fs ../../build-tools/git/hooks/pre-commit .git/hooks/pre-commit
+cd ${script_path} && command ln -fs ../../git-webdev-tools/git/hooks/pre-commit .git/hooks/pre-commit
 echo Done.
 echo " -------------------------------------------------------"
 
 
 echo -e " Setting up prepare-commit-msg hook... \c"
-cd ${script_path} && command ln -fs ../../build-tools/git/hooks/prepare-commit-msg .git/hooks/prepare-commit-msg
+cd ${script_path} && command ln -fs ../../git-webdev-tools/git/hooks/prepare-commit-msg .git/hooks/prepare-commit-msg
+echo Done.
+echo " -------------------------------------------------------"
+
+
+echo -e " Adding commit-msg hook for Gerrit Commit-Ids... \c"
+cd ${script_path} && command ln -fs ../../git-webdev-tools/git/hooks/commit-msg .git/hooks/commit-msg
 echo Done.
 echo " -------------------------------------------------------"
 
 
 echo " Running submodule setup scripts...."
 while read path; do
-	if [ -f "${script_path}/${path}/devsetup.sh" ] && [[ "${path}" != "build-tools" ]]; then
+	if [ -f "${script_path}/${path}/devsetup.sh" ] && [[ "${path}" != "git-webdev-tools" ]]; then
 		echo "	${script_path}/${path}..."
 		cd ${script_path}/${path} && command sh ./devsetup.sh && cd ${script_path}
 	fi
@@ -115,25 +118,26 @@ if [ -f "${script_path}/devsetup.conf" ]; then
 	echo " -------------------------------------------------------"
 	source ${script_path}/devsetup.conf
 
-	for (( i=1; i<=${#MODULE_NAME[@]}; i++ ))
+	for (( i=1; i<=${#MODULE_URL[@]}; i++ ))
 	do
-		if [ -d ${MODULE_DIR[$i]} ]; then
-			echo "	${MODULE_DIR[$i]} already exists."
+		if [ -d ${MODULE_PATH[$i]} ]; then
+			echo "	${MODULE_PATH[$i]} already exists."
 		else
-			echo -e "	Cloning ${MODULE_NAME[$i]} into ./${MODULE_DIR[$i]}/... \c"
+			echo -e "	Cloning ${MODULE_URL[$i]} into ./${MODULE_PATH[$i]}/... \c"
 
-			clone_command="git clone ssh://gerrit/${MODULE_NAME[$i]}"
+			clone_command="git clone ssh://gerrit/${MODULE_URL[$i]}"
 			if [[ -n ${BRANCH[$i]} && ${BRANCH[$i]} != "master" ]]; then
 				clone_command="${clone_command} --branch ${BRANCH[$i]}"
 			fi
 
-			command ${clone_command} ${MODULE_DIR[$i]}
-			if [ -f "${MODULE_DIR[$i]}/devsetup.sh" ]; then
-				cd ${script_path}/${MODULE_DIR[$i]} && command sh ./devsetup.sh && cd ${script_path}
+			command ${clone_command} ${MODULE_PATH[$i]}
+			if [ -f "${MODULE_PATH[$i]}/devsetup.sh" ]; then
+				cd ${script_path}/${MODULE_PATH[$i]} && command sh ./devsetup.sh && cd ${script_path}
 			fi
 
 			if [ $? -ne 0 ]; then
-				echo "There was a problem cloning ${MODULE_NAME[$i]}, please verify your SSH configuration: https://wiki.corp.returnpath.net/display/rpdev/Gerrit+FAQ#GerritFAQ-sshconfig"
+				echo "There was a problem cloning ${MODULE_URL[$i]}, please verify your git configuration and internet connection."
+				exit 1
 			else
 				echo Done.
 			fi
